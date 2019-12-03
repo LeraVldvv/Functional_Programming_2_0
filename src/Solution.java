6fbdd22d5c81b990b79ba2e4764a26d88fcd7c15
LeraVldvv <vvolodova@inbox.lv>
2019-12-03 13:27:22 +0200

    .

--- src/Solution.java
+++ src/Solution.java
@@ -1,45 +1,21 @@
+
 public class Solution {
-    private static boolean isPalindrome;
 
-    public static  void recursion(char[] a, int index) {
-         if (a.length % 2 == 0) {
-             if (index < (a.length / 2)) {
-                 if (a[index] == a[a.length - index - 1]) {
-                     isPalindrome=true;
-                     recursion(a, index + 1);
-                 } else {
-                     isPalindrome = false;
-                 }
-             }
-         }else{
-             if (index < ((a.length-1) / 2)) {
-                 if (a[index] == a[a.length - index - 1]) {
-                     isPalindrome=true;
-                     recursion(a, index + 1);
-                 } else {
-                      isPalindrome= false;
-                 }
-             }else if(index==(a.length-1) / 2){}
-         }
+
+    public static  boolean recursion(char[] a, int index) {
+        if ((a[index] == a[a.length - index - 1]) && (index < (a.length / 2)) ){
+            return recursion(a, index + 1);
+        } else if(a[index] != a[a.length - index - 1]){
+            return false;
+        } return true;
      }
-
-      public static void toLowerCaseMethod( char[]array,int index){
-        if(index<array.length-1) {
-            array[index]=Character.toLowerCase(array[index]);
-             toLowerCaseMethod(array,index+1);
-        }
-
-      }
         static boolean palindrome(String s) {
-         char[] charArray=  s.toCharArray();
+         char[] charArray=s.toCharArray();
          int startFromDigit=0;
-         toLowerCaseMethod(charArray,startFromDigit);
-         recursion(charArray,startFromDigit);
-         return isPalindrome;
+         return recursion(charArray,startFromDigit);
         }
 
     public static void main(String[] args) {
-     System.out.println(  palindrome("Abba"));
-
+     System.out.println(palindrome(("āl").toLowerCase()));
     }
 }
